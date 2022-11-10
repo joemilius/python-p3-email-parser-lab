5b5fe6d73df8fa37fbce4f6e27cc54bebc66bada
Joe Milius <joemilius@gmail.com>
2022-11-09 23:32:16 -0700

    completed sort of

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, string):
+        self.string = string
+        pass
+
+    def parse(self):
+        regex = re.compile(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b")
+        email_list =  regex.findall(self.string)
+        email_list.reverse()
+        return email_list
+        pass
+    pass
